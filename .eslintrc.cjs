c5af59b65677d35c0ac4ba8097b28d56b6bcb748
Bohdan Leonets <leonetsbogdan@gmail.com>
2023-07-13 14:13:19 +0300

    feat: add rules to eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,6 +22,46 @@ module.exports = {
       'MemberExpression': 1,
       'flatTernaryExpressions': false,
     }],
+    'vue/no-v-html': 0,
+    'vue/valid-v-on': 0,
+    'vue/multiline-html-element-content-newline': 0,
+    'vue/singleline-html-element-content-newline': 0,
+    'vue/html-closing-bracket-spacing': 0,
+    'vue/v-slot-style': 0,
+    'vue/html-self-closing': 2,
+    'vue/new-line-between-multi-line-property': 2,
+    'vue/max-attributes-per-line': ['error', {
+      'singleline': 4,
+    }],
+    'vue/no-multiple-template-root': 0,
+    'vue/require-default-prop': 0,
+    'vue/no-mutating-props': 0, // TODO: looks like it useful, prevent and fix warnings in future
+    'vue/html-indent': ['warn', 2],
+    'vue/no-v-text-v-html-on-component': 0,
+    'vue/valid-v-slot': ['error', { 'allowModifiers': true }],
+    'vue/multi-word-component-names': 1,
+    'vue/attributes-order': [2, {
+      'order': [
+        'DEFINITION',
+        'LIST_RENDERING',
+        'CONDITIONALS',
+        'RENDER_MODIFIERS',
+        'GLOBAL',
+        ['UNIQUE', 'SLOT'],
+        'TWO_WAY_BINDING',
+        'OTHER_DIRECTIVES',
+        'OTHER_ATTR',
+        'EVENTS',
+        'CONTENT',
+      ],
+      'alphabetical': false,
+    }],
+    'vue/match-component-file-name': [1, {
+      'extensions': ['vue', 'js'],
+    }],
+    'vue/component-name-in-template-casing': ['error', 'PascalCase', {
+      'registeredComponentsOnly': true,
+    }],
   },
   parserOptions: {
     ecmaVersion: 'latest'
