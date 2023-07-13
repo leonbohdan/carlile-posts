98e14e27e5187dd9860430bbf849821d0094b26e
Bohdan Leonets <leonetsbogdan@gmail.com>
2023-07-13 11:08:04 +0300

    feat: changed eslint and prettier rules, add additional changes to vite.config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,13 @@ module.exports = {
     // 2 - "error"
     'quotes': [2, 'single'],
     'object-curly-spacing': [2, 'always'],
+    'semi': [2, 'always'],
+    'comma-dangle': [2, 'always-multiline'],
+    'indent': ['error', 2, {
+      'SwitchCase': 1,
+      'MemberExpression': 1,
+      'flatTernaryExpressions': false,
+    }],
   },
   parserOptions: {
     ecmaVersion: 'latest'
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite'; 
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/carlile-posts/',
   plugins: [vue(), vueJsx()],
   server: {
-    port: 3000
+    port: 3000,
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
